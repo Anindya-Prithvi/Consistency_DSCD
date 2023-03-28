08e187841c8e34dddf1c5ffde4e63e9b97052a20
Andyyy <anindya20024@iiitd.ac.in>
2023-03-28 15:01:15 +0530

    writing success

--- src/tests/test.py
+++ src/tests/test.py
@@ -1,8 +1,10 @@
 import unittest
 import multiprocessing
 import logging
+import random
 import sys
 from time import sleep
+import uuid
 
 sys.path.append("../primary_backup")
 logger = logging.getLogger("test")
@@ -11,6 +13,7 @@ logger.setLevel(logging.INFO)
 class PBBP(unittest.TestCase):
     n=20
     process_list = []
+    client_list = []
         
     def test01_launch_registry_server(self):
         from registry_server import serve
@@ -42,7 +45,22 @@ class PBBP(unittest.TestCase):
     def test03_run_client(self):
         from client import Client
         c1 = Client(logger, "[::1]:1337")
+        self.client_list.append(c1)
         assert len(c1.KNOWN_SERVERS)==self.n, "All Servers not reg. or client fail"
+    
+    def test04_run_client_write(self):
+        # using first client
+        c1 = self.client_list[0]
+        replica = random.choice(c1.KNOWN_SERVERS)
+        file_uuid = str(uuid.uuid4())
+        filename = "I am Walter Hartwell White"
+        content = "I live in Albuquerque, New Mexico. I am 51 years old. I have a wife and two children. I am a high school chemistry teacher. I have terminal lung cancer. I am also a methamphetamine manufacturer. I am the one who knocks."
+        resp = c1.write_to_replica(replica, file_uuid, filename, content)
+        assert resp.status == "Success", "Write failed"
+        assert resp.uuid == file_uuid, "UUID mismatch"
+        assert len(resp.version)>0, "Version not set"
+        # can at most print resp.version, nothing to assert
+        
     
     def testzz_tear_down(self):
         for p in self.process_list:
